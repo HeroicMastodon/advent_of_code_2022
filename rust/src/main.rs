ed0ac5148e773d9a9350df69cc2455cf1b9625cf
HeroicMastodon <heroicmastodon@gmail.com>
2022-12-15 23:41:58 -0700

    some more cleanup

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,14 +2,7 @@
 use std::path::Path;
 use std::{fs, io};
 
-const BASE_URL: &str = "/home/garrick/Documents/personal/advent_of_code/src";
 mod day_01;
-mod day_10;
-mod day_11;
-mod day_12;
-mod day_13;
-mod day_14;
-mod day_15;
 mod day_02;
 mod day_03;
 mod day_04;
@@ -18,6 +11,12 @@ mod day_06;
 mod day_07;
 mod day_08;
 mod day_09;
+mod day_10;
+mod day_11;
+mod day_12;
+mod day_13;
+mod day_14;
+mod day_15;
 
 fn main() {
     day_01::day_1();
